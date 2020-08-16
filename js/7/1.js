61f7d300cc401cb6cb419cad9267d7fedf660204
AlexSyrov <1854105@gmail.com>
2020-08-17 00:12:30 +0300

    Доделал задачу из вебинара

--- js/7/1.js
+++ js/7/1.js
@@ -1,14 +1,17 @@
-let balance = 10000;
+let balance = 50000;
 const payment = 2000;
 const messagePr = `Общая стоимость зака ${payment} кредитов. Проверяем кол-во доступных средств на счету`;
 let message;
 
 console.log(messagePr);
 
-if (balance > payment) {
+if (payment <= balance) {
   balance - payment;
+  console.log('Ok');
   message = `На счету осталось ${balance - payment} кредитов.`;
 } else {
   message = `На счету не достаточно средств`;
 }
 console.log(message);
+
+console.log('Операция завершена');
